e52ff87e4b6687d620b3b2e1d3de8127c70dacb5
adritorafsan <adritorafsan@gmail.com>
2024-02-28 15:58:09 +0600

    yo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,8 +10,8 @@ const Hero = () => {
         <div className='space-y-2   py-2'>
           <p className='md:text-3xl text-xl text-slate-500 font-semibold dark:text-white '>Hello</p>
           <h1 className='dark:text-white md:text-4xl text-2xl'>I'm <span className='md:text-6xl text-3xl text-primary font-bold'>Adrito Rafsan</span></h1>
-          <h3 className='text-slate-500 text-lg font-semibold dark:text-white'>Learning Frontend With React Js and Tailwind Css</h3>
-          <p className='md:w-3/4 text-md text-gray-700 dark:text-white/80'>A 15 years old boy from Bangladesh . Try to code for his interest created by his brother Named Arian Zesan . A ui/ux Designer. They are on a mission . </p>
+          <h3 className='md:w-2/4 text-slate-500 text-lg font-semibold dark:text-white'>Learning Frontend With React Js and Tailwind Css . Already Created so many projects with HTML , CSS & JS</h3>
+          <p className='md:w-3/4 text-md text-gray-700 dark:text-white/80'>A 15 years old boy from Bangladesh . Try to code for his interest created by his brother Named Arian Zesan . A ui/ux Designer. They are on a mission . Now I'm in class 10 . SSC Batch-25 </p>
           <button href="https://www.youtube.com/channel/UCLUlzYryKHAEEzc0X-mX6hw" className='primary-btn'>Visit YT</button>
         </div>
 
