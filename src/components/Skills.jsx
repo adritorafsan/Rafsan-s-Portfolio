e68c0cd99aad10993999a62047b5f41f5b889d8e
adritorafsan <adritorafsan@gmail.com>
2024-03-11 15:32:06 +0600

    yo

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SkillsLevel from './SkillsLevel'
 
 const Skills = () => {
     return (
@@ -6,9 +7,14 @@ const Skills = () => {
             <section className="md:px-4 px-4 max-w-screen-2xl  mt-28 mx-auto">
                 <div className="container ">
                     <div className="flex md:flex-row flex-col justify-between items-center">
-                        <div className="md:w-2/4 dark:text-white">
-                            <SkillsLevel />
+                        <div className="md:w-1/4 w-full dark:text-white">
+                            <SkillsLevel  skillname="HTML" parcentage ="90%" />
+                            <SkillsLevel  skillname="CSS" parcentage ="93%" />
+                            <SkillsLevel  skillname="JAVASCRIPT" parcentage ="60%" />
+                            <SkillsLevel  skillname="REACT" parcentage ="78%" />
+                            <SkillsLevel  skillname="TAILWIND CSS" parcentage ="87%" />
                         </div>
+                        
                         <div className="relative">
                             <div className="text-center lg:text-7xl text-5xl dark:text-white/5 text-black/5 font-bold">SKILLS</div>
                             <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold dark:text-white/80 text-black/80 lg:text-2xl text-lg  ">Skills</h1>
---
+++ src/components/SkillsLevel.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SkillsLevel = ({skillname , parcentage}) => {
+  return (
+    <>
+    <div className='mt-4 space-y-2'>
+        <div className='flex justify-between items-center'>
+            <p className='font-bold text-md text-slate-600 dark:text-white'>{skillname}</p>
+            <p className='font-semibold text-[14px] text-slate-600 dark:text-white'>{parcentage}</p>
+        </div>
+        <div className='bg-slate-300 w-full rounded-full h-2'>
+            <div 
+            style={{ width : `${parcentage}`}}
+            className='bg-red-500 dark:bg-primary h-2 rounded-full '></div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default SkillsLevel
